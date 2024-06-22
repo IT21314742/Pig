518d5dbafe41a11393ed164bcb0311774f2f93e1
IT21314742 <it21314742@my.sliit.lk>
2024-06-22 18:42:48 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -29,7 +29,7 @@ print(player_scores)
 while max(player_scores) < max_score:
     
     for player_idx in range(players):
-        print("\nPlayer", player_idx + 1, "turn has just started!")
+        print("\nPlayer", player_idx + 1, "turn has just started!\n")
         current_score = 0
 
         while True:
